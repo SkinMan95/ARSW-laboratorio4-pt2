e92ac9899826f494f7d87f96f5d78afb99615f29
Alejandro Anzola <anzola95avila@gmail.com>
2018-03-08 19:40:15 -0500

    parte 2.1

--- js/OrdersController.js
+++ js/OrdersController.js
@@ -1,38 +1,105 @@
 var OrdersControllerModule = (function () {
 
-  var showOrdersByTable = function () {
-    //Todo implement
+    /* PRIVATE */
+    
+    var addTable = function (order) {
+	var tables = document.getElementById("order_tables");
 
-    var callback = {
+	var table = document.createElement("table");
+	table.setAttribute("id", "table" + order.table_id);
+	table.setAttribute("class", "orders_table");
 
-        onSuccess: function(ordersList){
-            //Todo implement
+	var tbody = document.createElement("tbody");
 
+	var tableHeader = document.createElement("tr");
+	tableHeader.setAttribute("class", "table_header");
+	
+	var headerRow = document.createElement("th");
+	headerRow.innerHTML = "Product";
+	tableHeader.appendChild(headerRow);
+	headerRow = document.createElement("th");
+	headerRow.innerHTML = "Quantity";
+	tableHeader.appendChild(headerRow);
+	headerRow = document.createElement("th");
+	headerRow.innerHTML = "Price";
+	tableHeader.appendChild(headerRow);
+	tbody.appendChild(tableHeader);
+
+	for(index = 0 ; index < order.products.length; index++) {
+	    var product = order.products[index];
+	    var row = document.createElement("tr");
+	    row.setAttribute("id", "table" + order.table_id + "_dish" + (index+1));
+	    row.setAttribute("class", "table_product");
+	    tbody.appendChild(row);
+	    
+	    var productName = document.createElement("td");
+	    productName.setAttribute("class", "product_name");
+	    productName.innerHTML = product.product;
+	    
+	    var productQuantity = document.createElement("td");
+	    productQuantity.setAttribute("class", "product_quantity");
+	    productQuantity.innerHTML = product.quantity;
+	    
+	    var productPrice = document.createElement("td");
+	    productPrice.setAttribute("class", "product_price");
+	    productPrice.innerHTML = product.price;
+	    
+	    row.appendChild(productName);
+	    row.appendChild(productQuantity);
+	    row.appendChild(productPrice);
+	}
+
+	table.appendChild(tbody);
+	tables.appendChild(table);
+    };
+
+    var clearTables = function () {
+	document.getElementById('order_tables').innerHTML = '';
+    };
+
+    /* PUBLIC */
+    
+    var showOrdersByTable = function () {
+	//Todo implement
+	console.log("Funciona");
+
+	var callback = {
+
+            onSuccess: function(ordersList){
+		// console.log(ordersList);
+		clearTables();
+		
+		for(var i in ordersList) {
+		    addTable(ordersList[i]);
+		}
             },
-        onFailed: function(exception){
-        //Todo implement
-        }
-    }
-    //RestaurantRestController.getOrders(callback)
-  };
+            onFailed: function(exception){
+		// TODO mejorar
+		console.log(exception);
+		alert('Fallo');
+            }
+	};
+	
+	RestControllerModule.getOrders(callback);
+    };
 
-  var updateOrder = function () {
-    // todo implement
-  };
+    var updateOrder = function () {
+	// todo implement
+    };
 
-  var deleteOrderItem = function (itemName) {
-    // todo implement
-  };
+    var deleteOrderItem = function (itemName) {
+	// todo implement
+    };
 
-  var addItemToOrder = function (orderId, item) {
-    // todo implement
-  };
+    var addItemToOrder = function (orderId, item) {
+	// todo implement
+    };
 
-  return {
-    showOrdersByTable: showOrdersByTable,
-    updateOrder: updateOrder,
-    deleteOrderItem: deleteOrderItem,
-    addItemToOrder: addItemToOrder
-  };
+    return {
+	showOrdersByTable: showOrdersByTable,
+	updateOrder: updateOrder,
+	deleteOrderItem: deleteOrderItem,
+	addItemToOrder: addItemToOrder
+    };
 
 })();
--- js/RestaurantRestController.js
+++ js/RestaurantRestController.js
@@ -1,26 +1,87 @@
 var RestControllerModule = (function () {
 
-  var getOrders = function (callback) {
-    // todo implement
-  };
+    /* PRIVATE */
+    
+    var DEFAULT_ADDRESS = 'http://localhost:8080'; // XXX
+    var server_url = DEFAULT_ADDRESS + '/orders';
+    
+    var getProductPrices = function () {
+	return axios.get(server_url + '/products');
+    };
 
-  var updateOrder = function (order, callback) {
-    // todo implement
-  };
+    var getOrdersFromServer = function () {
+	return axios.get(server_url);
+    };
 
-  var deleteOrder = function (orderId, callback) {
-    // todo implement
-  };
+    /**
+     * Construct an order in the javascript format
+     */
+    var constructOrder = function (order, products) {
+	var ord = {order_id:null, table_id:null, products:[]};
+	ord.order_id = order.tableNumber;
+	ord.table_id = order.tableNumber;
 
-  var createOrder = function (order, callback) {
-    // todo implement
-  };
+	for(p in order.orderAmountsMap) {
+	    var prod = {product: p, quantity: order.orderAmountsMap[p], price:null};
+	    prod.price = '$' + products.filter(function (e) {
+		return e.name === p;
+	    })[0].price;
 
-  return {
-    getOrders: getOrders,
-    updateOrder: updateOrder,
-    deleteOrder: deleteOrder,
-    createOrder: createOrder
-  };
+	    ord.products.push(prod);
+	}
+
+	return ord;
+    };
+
+    var constructOrders = function (orders, products) {
+	var res = [];
+	
+	for(i in orders) {
+	    ord = constructOrder(orders[i], products);
+	    res.push(ord);
+	}
+	
+	return res;
+    };
+
+    /* PUBLIC */
+
+    /**
+     * Gets the data from the server and calls the callback 
+     * methods onSuccess and onFailed respectively.
+     */
+    var getOrders = function (callback) {
+	axios.all([getProductPrices(), getOrdersFromServer()])
+	    .then(axios.spread( function (prices, orders) {
+		// console.log(products);
+		// console.log(orders.data);
+		products = prices.data;
+		var ords = constructOrders(orders.data, products);
+		callback.onSuccess(ords);
+	    }))
+	    .catch(function (error) {
+		console.log('ERROR: failed to get server data');
+		callback.onFailed(error);
+	    });
+    };
+
+    var updateOrder = function (order, callback) {
+	// todo implement
+    };
+
+    var deleteOrder = function (orderId, callback) {
+	// todo implement
+    };
+
+    var createOrder = function (order, callback) {
+	// todo implement
+    };
+
+    return {
+	getOrders: getOrders,
+	updateOrder: updateOrder,
+	deleteOrder: deleteOrder,
+	createOrder: createOrder
+    };
 
 })();
